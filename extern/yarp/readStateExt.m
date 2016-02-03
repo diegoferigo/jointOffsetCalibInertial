2ebfcfeb673f4b7ca2407bfdae4e33a964d38a2d
Nuno Guedelha <nuno.guedelha@iit.it>
2016-02-03 16:48:48 +0100

    Fixes: - aligned name of readState function with file name - removed feet sensors from sensor list - added comments - corrected captured data path - removed F/T sensors from parsing configuration structure - updated labels accordingly

--- extern/yarp/readStateExt.m
+++ extern/yarp/readStateExt.m
@@ -1,4 +1,4 @@
-function [q, dq, d2q, time] = readState(n, filename)
+function [q, dq, d2q, time] = readStateExt(n, filename)
 
 format = '%d %f ';
 fid    = fopen(filename);
@@ -16,9 +16,9 @@ for j = 1 : 10
 end
 C    = textscan(fid, format);
 time = C{1, 2};
-q    = cell2mat(C(1, 3    :3+  n-1));
-dq   = cell2mat(C(1, 3+  n:3+2*n-1));
-d2q  = cell2mat(C(1, 3+2*n:3+3*n-1));
+q    = cell2mat(C(1, 3    :3+  n-1)); % n columns of "q" value
+dq   = cell2mat(C(1, 3+  n:3+2*n-1)); % n columns of "dq" value
+d2q  = cell2mat(C(1, 3+2*n:3+3*n-1)); % n columns of "d2q" value
 
 [tu,iu] = unique(time);
 time    = tu';
